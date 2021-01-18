cafd03501c447f8948c376f025a8726c203f7336
kevinyohe <kevinyohe7@yahoo.com>
2021-01-18 15:28:32 -0500

    add details, view

--- server.js
+++ server.js
@@ -2,10 +2,10 @@ let express = require('express');
 let app = express();
 
 var pepper_list = [
-    { name: "Habernero", from: "eBay", soldby: "https://www.ebay.com/usr/theseedplant", image: "/images/habernero.jpg"},
-    { name: "Pimento", from: "eBay", soldby: "https://www.ebay.com/usr/southeastseeds", image: "/images/pimento.jpg"},
-    { name: "Peri Peri", from: "eBay", soldby: "https://www.ebay.com/usr/inheritedseeds", image: "/images/periperi.jpg"},
-    { name: "Thai Chilli DRAGON HEIRLOOM", from: "eBay", soldby: "https://www.ebay.com/usr/inheritedseeds", image: "/images/thaichili.jpg"},
+    { id: 1, name: "Habernero", from: "eBay", soldby: "https://www.ebay.com/usr/theseedplant", image: "/images/habernero.jpg"},
+    { id: 2, name: "Pimento", from: "eBay", soldby: "https://www.ebay.com/usr/southeastseeds", image: "/images/pimento.jpg"},
+    { id: 3, name: "Peri Peri", from: "eBay", soldby: "https://www.ebay.com/usr/inheritedseeds", image: "/images/periperi.jpg"},
+    { id: 4, name: "Thai Chilli DRAGON", from: "eBay", soldby: "https://www.ebay.com/usr/inheritedseeds", image: "/images/thaichili.jpg"},
 
 ]
 app.use( express.static( "public" ) ); 
@@ -14,6 +14,11 @@ app.set('view engine', 'ejs');
 app.get('/', (req, res) => {
   res.render('index', {foo: 'FOO', pepper_list: pepper_list});
 });
+
+app.get('/:id/', (req, res) => {
+    res.render('details', {pepper_details: "test"});
+  });
+  
 
 //app.listen(4000, () => console.log('Example app listening on port 4000!'));
 
